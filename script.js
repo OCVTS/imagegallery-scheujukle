fca3dce0b6b467f1ee9aa989a2bc2e8007cfa477
Mike B <34979@ocvts.org>
2025-01-10 13:58:53 +0000

    event

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-const galleryImages = document.querySelectorAll('#gallery-container img')
-const lightbox = document.getElementById('lightbox')
-const lightboxImg = document.getElementById('lightbox-img')
+const galleryImages = document.querySelectorAll('#gallery-container img') //all img in gallery
+const images = Array.from(galleryImages)
+const lightbox = document.getElementById('lightbox') //lightbox 
+const lightboxImg = document.getElementById('lightbox-img') //img highlighted
 
-galleryImages.forEach(image => {
+
+let currentIndex = 0;
+
+images.forEach((image, index) => {
     image.addEventListener('click', () => {
+        currentIndex = index;
         lightbox.classList.add("visible");
         lightboxImg.src = image.src
     })
 })
 
-lightbox.addEventListener('click', () => {
-    lightbox.classList.remove("visible")
+lightbox.addEventListener('click', (e) => {
+    if(e.target == lightbox) lightbox.classList.remove("visible")
 })
+
+function nextImg(){
+    currentIndex = (currentIndex + 1) % images.length;
+    console.log(currentIndex);
+    lightboxImg.src = images[currentIndex].src;
+}
+
+function lastImg(){
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    console.log(currentIndex);
+    lightboxImg.src = images[currentIndex].src;
+}
+
+document.addEventListener('keydown', (e) => {
+    if(e.code == 'ArrowLeft'){
+        lastImg()
+    }
+    if(e.code == 'ArrowRight'){
+        nextImg()
+    }
+})
+
